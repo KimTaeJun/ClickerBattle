cea0c384babcc18075df67255b1096a2eafbd033
ktjunkuku <ktjunkuku@naver.com>
2016-03-14 01:51:50 +0900

    a

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Server On!');
+});
+
 app.get('/', function(req,res){
   res.send('Hello! Babkoo Test');
 });
