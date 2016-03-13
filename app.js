b90dbccf3569493cb647a8e71b5c07d13d44d0aa
ktjunkuku <ktjunkuku@naver.com>
2016-03-14 02:53:40 +0900

    a

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
-
+///*
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('Server On!');
 });
-
-app.use(express.static(__dirname + '/public'));
+//*/
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function(){
   console.log('Server On!');
